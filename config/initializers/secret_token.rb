ee1205ab983ebedb5ad42f7a3180a68ccaab310c
johannesE <johannes.eifert@unifr.ch>
2013-10-18 15:33:51 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storysmoker::Application.config.secret_key_base = '8caebf897f25c1c654fc674bb83bf6f978da2bc3d5a29675b70a573839998df3bf2182a9ed67e5b50174f0b165e55deefd96dbe0acff7a5fd5cc5769edcb4ac6'

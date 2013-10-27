22448d209e2f33dd10ecc6c62e64fb54261d2176
johannesE <johannes.eifert@unifr.ch>
2013-10-27 11:23:33 +0100

    user management and authentication. I'll explain more in the next meeting. Try it out and find loopholes please.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131023112238) do
+ActiveRecord::Schema.define(version: 20131027100102) do
 
   create_table "snippets", force: true do |t|
     t.string   "content"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20131023112238) do
 
   create_table "stories", force: true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username",         null: false
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

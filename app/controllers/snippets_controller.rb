91f8a68d2c70a4eae83385eddd665a8fe5dd4f99
bmros <bl.rossier@bluewin.ch>
2013-10-29 17:14:04 +0100

    some links fixed

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,5 +1,12 @@
 class SnippetsController < ApplicationController
 
+  def index
+   @story = Story.find(params[:story_id])
+   @snippets = @story.snippets
+   #redirect_to root_path
+   
+  end
+
 
   def create
    @story = Story.find(params[:story_id])

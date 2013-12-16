cf55cfba22859e23c40dc39e9d17d75da1225fd3
bmros <bl.rossier@bluewin.ch>
2013-12-16 21:30:39 +0100

    design improve

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -13,7 +13,7 @@ class StoriesController < ApplicationController
   # GET /stories/1
   # GET /stories/1.json
   def show
-  end
+   @sortSnippets = @story.snippets.sort_by &:created_at  end
 
   # GET /stories/new
   def new
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,10 @@ class WelcomeController < ApplicationController
     @stories = Story.all
     @tags = Story.all.tag_counts_on(:tags)
 	
+	@randomStory = Story.where(status: 'finished').offset(rand(Story.where(status: 'finished').count)).first.snippets.sort_by &:created_at
+	
+
+	
 	unlockDB
 
   end
